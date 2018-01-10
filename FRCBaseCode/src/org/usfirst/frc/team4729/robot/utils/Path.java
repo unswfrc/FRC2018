d43f708b4c00c1fb9e26f5616525ef83673cd04a
B3NJP <b3np@pearcy.org>
2018-01-10 16:31:26 +1100

    Commented stuff out

--- FRCBaseCode/src/org/usfirst/frc/team4729/robot/utils/Path.java
+++ FRCBaseCode/src/org/usfirst/frc/team4729/robot/utils/Path.java
@@ -1,9 +1,9 @@
-package org.usfirst.frc.team4729.robot.utils;
-
-public class Path {
-	Point[] points;
-	
-	public Path(Point... data) {
-		points = data;
-	}
-}
+//package org.usfirst.frc.team4729.robot.utils;
+//
+//public class Path {
+//	Point[] points;
+//	
+//	public Path(Point... data) {
+//		points = data;
+//	}
+//}
