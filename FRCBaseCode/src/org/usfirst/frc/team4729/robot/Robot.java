1857492ceeb3312122e4a21736f91c85cd610207
iO Ninja <ioninja14@gmail.com>
2018-01-15 16:32:58 +1100

    basic start for auto
    THIS WAS DONE ON TEAM DESKTOP

--- FRCBaseCode/src/org/usfirst/frc/team4729/robot/Robot.java
+++ FRCBaseCode/src/org/usfirst/frc/team4729/robot/Robot.java
@@ -8,13 +8,6 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 
 import org.usfirst.frc.team4729.robot.subsystems.DriveSubsystem;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
- */
 public class Robot extends IterativeRobot {
 
 	public static DriveSubsystem driveSubsystem;
--- FRCBaseCode/src/org/usfirst/frc/team4729/robot/subsystems/Navigation.java
+++ FRCBaseCode/src/org/usfirst/frc/team4729/robot/subsystems/Navigation.java
@@ -1,31 +1,29 @@
-//package org.usfirst.frc.team4729.robot.subsystems;
-//
-//import org.usfirst.frc.team4729.robot.utils.Path;
-//import org.usfirst.frc.team4729.robot.utils.Point;
-//
-//import edu.wpi.first.wpilibj.command.Subsystem;
-//
-///**
-// *
-// */
-//
-//public class Navigation extends Subsystem {
-//    // Put methods for controlling this subsystem
-//    // here. Call these from Commands.
-//	
-//	Path path;
-//
-//    public void initDefaultCommand() {
-//        // Set the default command for a subsystem here.
-//        //setDefaultCommand(new MySpecialCommand());
-//    }
-//    
-//    public void setPath(Point... points) {
-//    	path = new Path(points);
-//    }
-//    
-//    public void reset() {
-//    	
-//    }
-//}
-//
+package org.usfirst.frc.team4729.robot.subsystems;
+
+import org.usfirst.frc.team4729.robot.utils.Path;
+import org.usfirst.frc.team4729.robot.utils.Point;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Navigation extends Subsystem {
+	
+	//variables
+	Path points;
+	String gameData;
+	
+    public void initDefaultCommand() {
+    	
+    }
+    
+    public void collectPoints() {
+    	gameData = DriverStation.getInstance().getGameSpecificMessage();
+		if(gameData.charAt(0) == 'L')
+		{
+			//Put left auto code here
+			
+		} else {
+			//Put right auto code here
+			
+		}
+}
+
