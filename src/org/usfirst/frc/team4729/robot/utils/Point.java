bc8628fae71bb29a92e29ea0ab41388643327834
Rowella <alex+github@gormand.com.au>
2018-01-17 11:49:52 +1100

    [Alex] Added all of the changes from the laptop version of the code

---
+++ src/org/usfirst/frc/team4729/robot/utils/Point.java
@@ -0,0 +1,15 @@
+//package org.usfirst.frc.team4729.robot.utils;
+//
+//public class Point {
+//	double x;
+//	double y;
+//	double angle;
+//	
+//	public double angleTo (Point point) {
+//		return 0; // TODO
+//	}
+//	
+//	public double distanceTO (Point point) {
+//		return 0; // TODO
+//	}
+//}
